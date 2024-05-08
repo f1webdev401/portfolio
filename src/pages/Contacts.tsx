4482fc287934f26bbf15c00ab6c0ca8911dc96c9
adarog999 <adarog999@gmail.com>
2024-05-08 12:16:07 +0800

    meta

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -6,15 +6,6 @@ const Contacts = () => {
             <div>
                 <h1>Contact Me 😊</h1>
             </div>
-            {/* <div className="phonenumber">
-                <h1>Phonenumber: </h1>
-                <p></p>
-            </div> */}
-            {/* <div className="facebook">
-                <h1>Facebook: </h1>
-                <a href="">F1 WEB DEV
-                </a>
-            </div> */}
             <div className="email">
                 <h1>Email: </h1>
                 <p>adarogn45@gmail.com</p>
