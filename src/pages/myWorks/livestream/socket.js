676deb371155a4f2042d8b0b8afeb7b004cab5b3
adarog999 <adarog999@gmail.com>
2024-05-26 00:50:06 +0800

    rerender2

--- src/pages/myWorks/livestream/socket.js
+++ src/pages/myWorks/livestream/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-// const socket = io('https://livestream-server-qhcr.onrender.com',{'multiplex':false,transports: ['websocket']})
-const socket = io('http://localhost:4000',{'multiplex':false,transports: ['websocket']})
+const socket = io('https://livestream-server-qhcr.onrender.com',{'multiplex':false,transports: ['websocket']})
+// const socket = io('http://localhost:4000',{'multiplex':false,transports: ['websocket']})
 export default socket
